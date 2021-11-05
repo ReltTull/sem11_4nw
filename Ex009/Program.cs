98eba099950351e3c4c74b7c4ea0ade18a22842b
ReltTull <92668493+ReltTull@users.noreply.github.com>
2021-11-05 17:11:11 +0300

    Ex009compl

--- Ex009/Program.cs
+++ Ex009/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Показать последнюю цифру трёхзначного числа
+int a = 234;
+int tens = a % 100;
+int units = tens % 10;
+Console.WriteLine(units);

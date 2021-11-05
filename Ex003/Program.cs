26aa1ca8d2a30e2be669cdfdf19a052e9552aad2
ReltTull <92668493+ReltTull@users.noreply.github.com>
2021-11-05 15:39:47 +0300

    ex003comple

---
+++ Ex003/Program.cs
@@ -0,0 +1,32 @@
+﻿//  3.По введенному номеру дня недели вывести его название
+
+Console.Write("Print the serial number of the day of the week: ");
+string dayoftheweek = Console.ReadLine();
+if (dayoftheweek == "1")
+{
+    Console.WriteLine("monday");
+}
+if (dayoftheweek == "2")
+{
+    Console.WriteLine("tuesday");
+}
+if (dayoftheweek == "3")
+{
+    Console.WriteLine("wednesday");
+}
+if (dayoftheweek == "4")
+{
+    Console.WriteLine("thursday");
+}
+if (dayoftheweek == "5")
+{
+    Console.WriteLine("friday");
+}
+if (dayoftheweek == "6")
+{
+    Console.WriteLine("saturday");
+}
+if (dayoftheweek == "7")
+{
+    Console.WriteLine("sunday");
+}

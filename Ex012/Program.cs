405ec307f4186707dc67e68a454747fc3fbe834e
ReltTull <92668493+ReltTull@users.noreply.github.com>
2021-11-08 19:48:18 +0300

    Ex012added

---
+++ Ex012/Program.cs
@@ -0,0 +1,11 @@
+﻿//12. Удалить вторую цифру трёхзначного числа
+int n = new Random().Next(100, 1000);
+System.Console.WriteLine(n);
+int withoutsecond(int n)
+{
+    int first = n / 100;
+    int third = n % 10;
+    return first * 10 + third;
+
+}
+Console.WriteLine(withoutsecond(n));

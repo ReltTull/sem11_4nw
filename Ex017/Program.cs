5acc38df300d7a307351f03af1317162fd035b21
ReltTull <92668493+ReltTull@users.noreply.github.com>
2021-11-08 19:50:06 +0300

    ex017added

---
+++ Ex017/Program.cs
@@ -0,0 +1,16 @@
+﻿// 17. По двум заданным числам проверять является ли одно квадратом другого
+
+int a = 5;
+int b = 16;
+if (a*a == b)
+{
+    System.Console.WriteLine($"{b} - квадрат числа {a}");
+}
+else
+{
+    if (b*b == a)
+    {
+        System.Console.WriteLine($"{a} - квалрат числа {b}");
+    }
+    else {System.Console.WriteLine("числа не являются квадратами друг друга");}
+}

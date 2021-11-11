987ea22e2ccb66eb30b47fd3d9ea4e9d75c2c9d5
ReltTull <92668493+ReltTull@users.noreply.github.com>
2021-11-11 12:25:36 +0300

    ex019added

---
+++ Ex019/Program.cs
@@ -0,0 +1,7 @@
+﻿// Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+int x = -1;
+int y = 2;
+if (x > 0 & y > 0) {System.Console.WriteLine("first quart");}
+if (x > 0 & y < 0) {System.Console.WriteLine("second quart");}
+if (x < 0 & y < 0) {System.Console.WriteLine("third quart");}
+if (x < 0 & y > 0) {System.Console.WriteLine("fourth quart");}

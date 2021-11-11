44c26bc0dd95889ae1bd5859b768b44cc45898b4
ReltTull <92668493+ReltTull@users.noreply.github.com>
2021-11-11 12:24:16 +0300

    ex016 edited

--- Ex016/Program.cs
+++ Ex016/Program.cs
@@ -1,9 +1,28 @@
 ﻿// 16. Дано число обозначающее день недели. Выяснить является номер дня недели выходным
 
+// System.Console.WriteLine("Print number (1-7)");
+// string b = Console.ReadLine();
+// bool isweekend(string a)
+// {
+// return (a == "6" | a== "7");
+// }
+// System.Console.WriteLine(isweekend(b));
+
+// System.Console.WriteLine("Print number (1-7)");
+// string a = Console.ReadLine();
+// if (a == "6" | a== "7") // "|" - оператор "или".
+// {
+//     System.Console.WriteLine("Weekend number");
+// }
+// else {System.Console.WriteLine("not a weekend number");}
+
 System.Console.WriteLine("Print number (1-7)");
-string a = Console.ReadLine();
-if (a == "6" | a== "7") // "|" - оператор "или".
+string b = Console.ReadLine();
+if (b == "6" | b == "7")
 {
-    System.Console.WriteLine("Weekend number");
+    System.Console.WriteLine("weekend");
 }
-else {System.Console.WriteLine("not a weekend number");}
+if (b != "6" & b != "7")
+{
+    System.Console.WriteLine("not a weeknd");
+}

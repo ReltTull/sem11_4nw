f6bef3ca21a769ecf89db87219496ea612213510
ReltTull <92668493+ReltTull@users.noreply.github.com>
2021-11-11 12:26:07 +0300

    ex020added

---
+++ Ex020/Program.cs
@@ -0,0 +1,19 @@
+﻿// 20. Задать номер четверти, показать диапазоны для возможных координат
+System.Console.Write("Введите номер четверти: ");
+int q = int.Parse(Console.ReadLine());
+if (q == 1)
+{
+    System.Console.WriteLine("x > 0, y > 0");
+}
+if (q == 2)
+{
+    System.Console.WriteLine("x > 0, y < 0");
+}
+if (q == 3)
+{
+    System.Console.WriteLine("x < 0, y < 0");
+}
+if (q == 4)
+{
+    System.Console.WriteLine("x < 0, y > 0");
+}

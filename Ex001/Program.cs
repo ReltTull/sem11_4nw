8e47f9b9e24f4e13e1afd61fc5a5fc59ab9d5dfa
ReltTull <92668493+ReltTull@users.noreply.github.com>
2021-11-11 12:15:57 +0300

    ex001edit

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -1,12 +1,7 @@
 ﻿//  1. По двум введённым числам проверять является ли первое квадратом второго
 
-int a = 9;
-int b = 3;
-if (b * b == a)
+bool issqr(int a, int b)
 {
-    Console.WriteLine(b + " is the root of the number " + a);
+    return (b * b == a);
 }
-else
-{
-    Console.WriteLine(b + " isn't the root of the number " + a);
-}
+System.Console.WriteLine(issqr(4,2));

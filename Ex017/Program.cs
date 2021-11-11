cdba0d023ba2427cd8432d222378167c75ebeb49
ReltTull <92668493+ReltTull@users.noreply.github.com>
2021-11-11 12:24:37 +0300

    ex017edited

--- Ex017/Program.cs
+++ Ex017/Program.cs
@@ -1,16 +1,43 @@
 ﻿// 17. По двум заданным числам проверять является ли одно квадратом другого
 
-int a = 5;
-int b = 16;
+// bool issqr(int a, int b)
+// {
+//     return (a*a == b) & (b*b == a);
+// }
+// System.Console.WriteLine(issqr(4,2));
+
+
+// int a = 4;
+// int b = 2;
+// if (a*a == b)
+// {
+//     System.Console.WriteLine($"{b} - квадрат числа {a}");
+// }
+// else
+// {
+//     if (b*b == a)
+//     {
+//         System.Console.WriteLine($"{a} - квалрат числа {b}");
+//     }
+//     else {System.Console.WriteLine("числа не являются квадратами друг друга");}
+// }
+
+int issqr(int a, int b, string text)
+{
 if (a*a == b)
 {
-    System.Console.WriteLine($"{b} - квадрат числа {a}");
+    text = $"{b} - квадрат числа {a}";
+    return text;
 }
 else
 {
     if (b*b == a)
     {
-        System.Console.WriteLine($"{a} - квалрат числа {b}");
+        text = $"{a} - квалрат числа {b}";
+        return text;
     }
     else {System.Console.WriteLine("числа не являются квадратами друг друга");}
 }
+return text;
+}
+System.Console.WriteLine(issqr(4,16));

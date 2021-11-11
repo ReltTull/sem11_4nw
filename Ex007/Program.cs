dc01dfb6f19cb78575ced055f1252788e69cbe76
ReltTull <92668493+ReltTull@users.noreply.github.com>
2021-11-11 12:17:14 +0300

    ex007edited

--- Ex007/Program.cs
+++ Ex007/Program.cs
@@ -1,18 +1,68 @@
 ﻿// 7. Показать числа от -N до N
-
-int n = -7;
-int index = n;
-while (index < -n)
-{
-    Console.Write(index + " ");
-    index++;
-}
-
-// int [] array = new int [10];
-// void CreateArray(int [] array)
+// int ShowNumbers(int n1)
 // {
-//     for(int i = 0; i < 10; i++)
+//     int n = n1;
+//     int index = -n1;
+//     while (index <= -n)
 //     {
-//         array[]
+//         System.Console.WriteLine(index);
+//         index++;
 //     }
+//     return index;
+// }
+// System.Console.WriteLine(ShowNumbers(-7));
+
+int ShowNumbers(int n1)
+{
+    int n = n1;
+    int index = -n1;
+    while (index <= -n)
+    {
+        System.Console.WriteLine(index);
+        index++;
+    }
+    return index;
+}
+System.Console.WriteLine(ShowNumbers(-7));
+
+
+// bool IsPolindrome(int N)
+// {
+//     string C = Convert.ToString(N);
+//     int N1 = C[0];
+//     int N2 = C[1];
+//     int N3 = C[3];
+//     int N4 = C[4];
+//     if (N1 == N4 & N2 == N3)
+//     {
+//         return true;
+//     }
+//     else
+//     {
+//         return false;
+//     }
+// }
+// bool c = IsPolindrome (21312);
+// System.Console.WriteLine(c);
+
+
+
+
+// for(index = n; index < -n; index++)
+// int CreateArray(int [] array)
+// {
+//     int n = n1;
+//     int index = n;
+//     for(index = n; index < -n; index++)
+//     {
+//         array[index + " "];
+//     }
+// }
+
+// int n = -7;
+// int index = n;
+// while (index <= -n)
+// {
+//     Console.Write(index + " ");
+//     index++;
 // }

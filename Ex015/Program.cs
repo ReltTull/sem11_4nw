633b73c7c53cf342b2c45df9fca0a9a072be1ef6
ReltTull <92668493+ReltTull@users.noreply.github.com>
2021-11-11 12:23:58 +0300

    ex015edited

--- Ex015/Program.cs
+++ Ex015/Program.cs
@@ -1,8 +1,14 @@
 ﻿// 15. Дано число. Проверить кратно ли оно 7 и 23 
 
-int a = 161;
-if (a % 7 == 0 & a % 23 ==0)
+bool ismultiplicity(int a)
 {
-    System.Console.WriteLine("yes");
+    return (a % 7 == 0 & a % 23 ==0);
 }
-else {System.Console.WriteLine("no");}
+System.Console.WriteLine(ismultiplicity(161));
+
+// int a = 161;
+// if (a % 7 == 0 & a % 23 ==0)
+// {
+//     System.Console.WriteLine("yes");
+// }
+// else {System.Console.WriteLine("no");}

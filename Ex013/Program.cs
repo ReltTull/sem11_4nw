dea1b840571e1a3cc09c6fec837905ef9f40e288
ReltTull <92668493+ReltTull@users.noreply.github.com>
2021-11-11 12:23:18 +0300

    ex013edited

--- Ex013/Program.cs
+++ Ex013/Program.cs
@@ -1,16 +1,15 @@
 ﻿// 13. Выяснить, кратно ли число заданному, если нет, вывести остаток.
 
-// int a = 80;
-// int b = 20;
-// System.Console.WriteLine(a);
-// System.Console.WriteLine(b);
-// int result = a / b;
-// if (a % b ==0)
-// {
-//     System.Console.WriteLine("Кратно");
-// }
-// else
-// {
-//     System.Console.WriteLine("Остаток = " + a % b);
-// }
 
+int a = new Random ().Next(10,20);
+int b = 2;
+System.Console.WriteLine(a);
+System.Console.WriteLine(b);
+if (a % b ==0)
+{
+    System.Console.WriteLine("Кратно");
+}
+else
+{
+    System.Console.WriteLine("Остаток = " + a % b);
+}

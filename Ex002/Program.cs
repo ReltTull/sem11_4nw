74d64353af62ee308722936465f5feb6ae878901
ReltTull <92668493+ReltTull@users.noreply.github.com>
2021-11-11 12:15:21 +0300

    ex002added

---
+++ Ex002/Program.cs
@@ -0,0 +1,15 @@
+﻿// 2. Даны два числа. Показать большее и меньшее число
+int G = new Random().Next(10, 100);
+System.Console.WriteLine(G);
+char GetMaxNumber(int G)
+{
+    string ab = Convert.ToString(G);
+    char G1 = ab[0];
+    char G2 = ab[1];
+    if (G1 > G2) {return G1;}
+    else {return G2;}
+}
+char M = GetMaxNumber(G);
+Console.WriteLine($"В числе {G} наибольшая цифра - {GetMaxNumber(G)}");
+
+// char - ?

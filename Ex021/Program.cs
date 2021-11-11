beea5b8e785faaf901cc51a4eb0fc48a07b61cfe
ReltTull <92668493+ReltTull@users.noreply.github.com>
2021-11-11 12:26:24 +0300

    ex021added

---
+++ Ex021/Program.cs
@@ -0,0 +1,25 @@
+﻿
+int N = new Random().Next(12321, 12323);
+System.Console.WriteLine(N);
+bool IsPolindrome(int N)
+{
+    string C = Convert.ToString(N);
+    int N1 = C[0];
+    int N2 = C[1];
+    int N3 = C[3];
+    int N4 = C[4];
+    if (N1 == N4 & N2 == N3)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+System.Console.WriteLine(IsPolindrome (N));
+
+// if (IsPolindrome(12312)) Console.WriteLine("Палиндром.");
+// else Console.WriteLine("Не палиндром.");
+
+

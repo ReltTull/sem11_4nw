621a0833e4be6462565c88b497e9402dccdd2bc9
ReltTull <92668493+ReltTull@users.noreply.github.com>
2021-11-18 14:27:51 +0300

    Ex038added

---
+++ Ex038/Program.cs
@@ -0,0 +1,21 @@
+﻿// 38. Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+int[] GetSumOfEvenNumb(int size)
+{
+    int[] RandArr = new int[size];
+    int sum = 0;
+    for(int i = 0; i < size; i++)
+    {
+        RandArr[i] = new Random().Next(-10,10);
+        System.Console.Write(RandArr[i] + " ");
+        if(i%2 != 0)
+        {
+            sum = sum + RandArr[i];
+        }
+        else {}
+    }
+    System.Console.WriteLine();
+    System.Console.WriteLine(sum);
+    return RandArr;
+}
+System.Console.WriteLine(GetSumOfEvenNumb(7));

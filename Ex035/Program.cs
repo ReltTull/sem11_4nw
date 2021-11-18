2d37178e0f3d9826f38f91838cf4adf6904bab5e
ReltTull <92668493+ReltTull@users.noreply.github.com>
2021-11-18 14:26:55 +0300

    Ex034added

---
+++ Ex035/Program.cs
@@ -0,0 +1,19 @@
+﻿// 35. Определить, присутствует ли в заданном массиве, некоторое число 
+int[] array = new int[10];
+for (int i = 0; i < 10; i++)
+{
+    array[i] = new Random().Next(1, 6);
+    Console.Write(array[i] + " ");
+}
+System.Console.WriteLine();
+int find = 3;
+int count = 0;
+for (int i = 0; i < 10; i++)
+{
+    if (find == array[i])
+    {
+        count = count + 1;
+    }
+    else {}
+}
+System.Console.WriteLine($"{find} is using in array {count} times");

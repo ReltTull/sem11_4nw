6b23bae8fdb9964a0a3db19035a9bccb2fb35e79
ReltTull <92668493+ReltTull@users.noreply.github.com>
2021-11-18 14:24:53 +0300

    Ex031added

---
+++ Ex031/Program.cs
@@ -0,0 +1,18 @@
+﻿// 31. Задать массив из 8 элементов и вывести их на экран 
+
+int[] ShowArray(int ArrLenght)
+{
+int[] array = new int [ArrLenght];
+for (int i = 0; i < ArrLenght; i++)
+{
+    Console.Write(array[i] + " ");
+}
+return array;
+}
+System.Console.WriteLine(ShowArray(8));
+
+int[] ExpectedArray = {0, 0, 0, 0, 0, 0, 0, 0};
+int[] ActualArray = ShowArray(8);
+
+if (ActualArray.Length == ExpectedArray.Length) System.Console.WriteLine("Корректно");
+else System.Console.WriteLine("Не корректно");

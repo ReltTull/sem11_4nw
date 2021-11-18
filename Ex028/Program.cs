5897ec7818b5c1a6426f4c3919217392e7b3d39c
ReltTull <92668493+ReltTull@users.noreply.github.com>
2021-11-18 14:22:37 +0300

    Ex028added

---
+++ Ex028/Program.cs
@@ -0,0 +1,24 @@
+﻿// // 28. Подсчитать сумму цифр в числе
+// int full = 123;
+// int sum = 0;
+// while (full > 0)
+// {
+//     sum = sum + full%10;
+//     full = full/10;
+// }
+// System.Console.WriteLine(sum);
+
+int SumOfNumbers(int full)
+{
+    int sum = 0;
+    while (full > 0)
+    {
+        sum = sum + full%10;
+        full = full/10;
+    }
+    return sum;
+}
+System.Console.WriteLine(SumOfNumbers(123));
+
+if (7 == SumOfNumbers(123)) System.Console.WriteLine("Корректно");
+else System.Console.WriteLine("Не корректно");

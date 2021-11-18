235233a8b8623e2d6414255836a8b0ec0a042858
ReltTull <92668493+ReltTull@users.noreply.github.com>
2021-11-18 14:28:03 +0300

    Ex039added

---
+++ Ex039/Program.cs
@@ -0,0 +1,32 @@
+﻿// 39. Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, 
+// второй и предпоследний и т.д.
+
+// int[] InitialArray = { 1, 5, 2, 7, 9, 3, 6, 8 };
+// for (int s = 0; s < InitialArray.Length / 2; s++)
+// {
+//     int sum = InitialArray[0 + s] + InitialArray[InitialArray.Length - 1 - s];
+//     Console.WriteLine(sum);
+// }
+
+int[] GetRandArr(int size)
+{
+    int[] RandArray = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        RandArray[i] = new Random().Next(1, 10);
+        System.Console.Write(RandArray[i] + " ");
+    }
+    return RandArray;
+}
+
+int GetSumOfArrElem(int[] array)
+{
+    for (int s = 0; s < array.Length / 2; s++)
+    {
+        int sum = array[0 + s] + array[array.Length - 1 - s];
+        System.Console.WriteLine(sum);
+    }
+    return -1;
+}
+System.Console.WriteLine(GetSumOfArrElem(GetRandArr(8)));
+System.Console.WriteLine(GetRandArr(8));

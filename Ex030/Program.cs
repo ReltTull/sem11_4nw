7caea50a2b141382c8a1b65f7901d4397cf81abb
ReltTull <92668493+ReltTull@users.noreply.github.com>
2021-11-18 14:23:27 +0300

    Ex030added

---
+++ Ex030/Program.cs
@@ -0,0 +1,24 @@
+﻿// 30. Показать кубы чисел, заканчивающихся на четную цифру
+
+// int CubeOfNumber(int number, int N)
+// {
+//     while (number < N)
+//     {
+//         number = number * number * number;
+//         number ++;
+//     }
+//     return number;
+// }
+// System.Console.WriteLine(CubeOfNumber(1,5));
+
+int number = 1;
+int N = 10;
+for (number = 1; number <= N; number++)
+{
+    int result = number * number * number;
+    if (result % 2 == 0)
+    {
+        System.Console.WriteLine(result);
+    }
+}
+

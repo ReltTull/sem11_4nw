d966551539beaaf0140d25f39b5e4ef7d1b6faca
ReltTull <92668493+ReltTull@users.noreply.github.com>
2021-11-18 14:21:43 +0300

    Ex025added

---
+++ Ex025/Program.cs
@@ -0,0 +1,19 @@
+﻿// 25. Найти сумму чисел от 1 до А
+// int A = 10;
+// int sum = 0;
+// for (int number = 1; number < A; number++)
+// {
+//     sum = sum + number;
+// }
+// System.Console.WriteLine(sum);
+
+int FindSum(int A)
+{
+    int sum = 0;
+    for (int number = 1; number < A; number++)
+    {
+        sum = sum + number;
+    }
+    return sum;
+}
+System.Console.WriteLine(FindSum(7));

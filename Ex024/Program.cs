5baf98e9576b23bfd9605f858529355a4f6c55cb
ReltTull <92668493+ReltTull@users.noreply.github.com>
2021-11-18 14:21:28 +0300

    Ex024added

---
+++ Ex024/Program.cs
@@ -0,0 +1,20 @@
+﻿// 24. Найти кубы чисел от 1 до N
+// int FindCube(int number, int N)
+// {
+//     for (number = 1; number < N; number++)
+//     {
+//         int result = number * number * number;
+//         return result;
+//     }
+//     return N;
+// }
+// System.Console.WriteLine(FindCube(1,5));
+
+int number = 1;
+int N = 5;
+for (number = 1; number <= N; number++)
+{
+    System.Console.Write($"Куб числа {number} - ");
+    number = number * number * number;
+    System.Console.WriteLine(number);
+}

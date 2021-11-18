914404bbfd532a27097da594f43db92267033020
ReltTull <92668493+ReltTull@users.noreply.github.com>
2021-11-18 14:22:18 +0300

    Ex026added

---
+++ Ex026/Program.cs
@@ -0,0 +1,22 @@
+﻿// 26. Возведите число А в натуральную степень B используя цикл
+
+int DegreeNumberToDegree(int number, int degree)
+{
+    int result = 1;
+    for (int i = 1; i <= degree; i++);
+    {
+        result = result * number;
+    }
+    return result;
+}
+System.Console.WriteLine(DegreeNumberToDegree(2,3));
+
+
+if (8 == DegreeNumberToDegree(2,3))
+{
+    System.Console.WriteLine("Correct");
+}
+else 
+{
+    System.Console.WriteLine("Uncorrect");
+}

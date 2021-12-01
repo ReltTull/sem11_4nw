aab5cf34f2c09921274bb3fea9a52bac9a640d5a
ReltTull <92668493+ReltTull@users.noreply.github.com>
2021-12-01 18:40:13 +0300

    ex043 added

---
+++ Ex043/Program.cs
@@ -0,0 +1,20 @@
+﻿// 43. Написать программу преобразования десятичного числа в двоичное
+
+// int number = 334;
+// int d10 = 1;
+// int result = 0;
+// while (number != 0)
+// {
+//     result = result + number % 2 * d10;
+//     number= number /2;
+//     d10 = d10 * 10;
+// }
+// System.Console.WriteLine(result);
+
+int number = 251; 
+int clss = 1;
+int result = 0;
+while (number > 2)
+{
+    result = number % 2 * clss;
+}

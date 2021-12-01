16cec57072308614c45b3e9540d16d108351d2ce
ReltTull <92668493+ReltTull@users.noreply.github.com>
2021-12-01 18:41:56 +0300

    ex049 added

---
+++ Ex049/Program.cs
@@ -0,0 +1,27 @@
+﻿// 49. Показать двумерный массив размером m×n заполненный вещественными числами
+double[,] GetArray(int a, int b)
+{
+    double[,] array = new double[a, b];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().NextDouble() * 10;
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+double[,] array1 = GetArray(5, 7);
+PrintArray(array1);

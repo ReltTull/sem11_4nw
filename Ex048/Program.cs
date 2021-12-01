87ac223574844762022dff7279d0a2d7e760e7f1
ReltTull <92668493+ReltTull@users.noreply.github.com>
2021-12-01 18:41:43 +0300

    ex048 added

---
+++ Ex048/Program.cs
@@ -0,0 +1,27 @@
+﻿// 48. Показать двумерный массив размером m×n заполненный целыми числами
+int[,] GetArray(int a, int b)
+{
+    int[,] array = new int[a, b];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-10, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+int[,] array1 = GetArray(5, 7);
+PrintArray(array1);

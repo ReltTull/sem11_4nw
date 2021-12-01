7d8c671fbcf8be865b5a863b63a0ccf4c61ed2b1
ReltTull <92668493+ReltTull@users.noreply.github.com>
2021-12-01 18:42:48 +0300

    ex051 added

---
+++ Ex051/Program.cs
@@ -0,0 +1,27 @@
+﻿// 51. Задать двумерный массив следующим правилом: Aₘₙ = m+n
+int[,] GetArray(int a, int b)
+{
+    int[,] array = new int[a, b];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+int[,] array1 = GetArray(5, 7);
+PrintArray(array1);
